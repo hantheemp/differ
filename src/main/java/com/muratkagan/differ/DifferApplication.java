8f2c230f93ad0c8a57341cc76bfa8789bf13099a
Murat Kağan <98017089+hantheemp@users.noreply.github.com>
2026-01-23 22:35:25 +0300

    Add initial project structure and build artifacts for Differ application
    - Created pom.properties for Maven archiver with artifact details.
    - Added createdFiles and inputFiles lists for the Maven compiler plugin.
    - Generated original JAR file for the Differ application including compiled classes and metadata.

---
+++ src/main/java/com/muratkagan/differ/DifferApplication.java
@@ -0,0 +1,11 @@
+package com.muratkagan.differ;
+
+import javax.swing.SwingUtilities;
+
+public class DifferApplication {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			System.out.println("Differ starting...");
+		});
+	}
+}
