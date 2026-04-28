1a2c3f16598fb59f34c9068e2e14c787cb93a78d
Murat Kağan <98017089+hantheemp@users.noreply.github.com>
2026-04-28 09:56:57 +0300

    build: migrate MVP to Electron + Vite for native Monaco and FS support

--- differ/src/main/ipc/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerDirectoryIpc } from './directory'
-import { registerFileIpc } from './file'
-import { registerIgnoreIpc } from './ignore'
-
-export function registerIpcHandlers() {
-  registerDirectoryIpc()
-  registerFileIpc()
-  registerIgnoreIpc()
-}
--- differ/src/renderer/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonProps } from './types'
-
-export default function Button({ children, className = '', ...props }: ButtonProps) {
-  return (
-    <button className={className} {...props}>
-      {children}
-    </button>
-  )
-}
